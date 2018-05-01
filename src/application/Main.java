dda8d1b8e53f1c056e554567f04ec9041991a76c
Javier <Javier@DRJ>
2018-05-01 19:37:17 +0100

    update

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -15,6 +19,11 @@ public class Main extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("/application/Example.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			//FileInputStream input = new FileInputStream("/application/oracle-java-javafx-jdk.jpg");
+	        //Image image = new Image(input);
+	        //ImageView imageView = new ImageView(image);
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- src/controller/InicioController.java
+++ src/controller/InicioController.java
@@ -8,7 +8,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import Samples.animation.Animation;
+import Samples.canvastest.CanvasTest;
 import Samples.colorfulcircles.*;
+import Samples.helloeffects.HelloEffects;
+import Samples.login.Login;
 import Samples.webviewsample.WebViewSample;
 
 public class InicioController {
@@ -34,16 +38,44 @@ public class InicioController {
     @FXML
     void showExample1(ActionEvent event) {
     	ColorfulCircles sample1 = new ColorfulCircles();
-    	Stage p = new Stage();
-		sample1.start(p);
+    	Stage primaryStage = new Stage();
+		sample1.start(primaryStage);
     }
     
     @FXML
     void showExample2(ActionEvent event) {
     	WebViewSample sample2 = new WebViewSample();
-    	Stage p = new Stage();
-		sample2.start(p);
+    	Stage primaryStage = new Stage();
+		sample2.start(primaryStage);
     }
 
-
+    @FXML
+    void showExample3(ActionEvent event) {
+    	Login sample3 = new Login();
+    	Stage primaryStage = new Stage();
+		sample3.start(primaryStage);
+    }
+    
+    @FXML
+    void showExample4(ActionEvent event) {
+    	Animation sample4 = new Animation();
+    	Stage primaryStage = new Stage();
+		sample4.start(primaryStage);
+    }
+    
+    @FXML
+    void showExample5(ActionEvent event) {
+    	HelloEffects sample5 = new HelloEffects();
+    	Stage primaryStage = new Stage();
+			sample5.start(primaryStage);
+    }
+    
+    
+    @FXML
+    void showExample6(ActionEvent event) {
+    	CanvasTest sample6 = new CanvasTest();
+    	Stage primaryStage = new Stage();
+			sample6.start(primaryStage);
+    }
+    
 }
